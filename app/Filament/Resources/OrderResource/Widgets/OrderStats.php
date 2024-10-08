0f3ddee8a904aafd08a17a923ccb7122ec721c70
hamdan266 <sehahh26@gmail.com>
2024-10-09 02:03:56 +0700

    "Added OrderStats widget and implemented tabs with filtering by order status on ListOrders page"

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -2,15 +2,26 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class OrderStats extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('New Order', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'), 'IDR'))
         ];
     }
 }
+
+
+
+
+
+// Cara membuat Widget "php artisan make:filament-widget OrderStats --resource=OrderResource"
