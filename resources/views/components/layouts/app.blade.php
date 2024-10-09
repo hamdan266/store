88443b3d7a406b9a3ff62fbab6a8377b684519c3
hamdan266 <sehahh26@gmail.com>
2024-10-09 22:54:08 +0700

    "Updated Livewire components and views to display categories and brands dynamically from database"

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'hamdhann' }}</title>
+        <title>{{ $title ?? 'hamdanStore' }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
