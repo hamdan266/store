f1ef3d83d979b7c423a72fe24a6754b9c3a50b42
hamdan266 <sehahh26@gmail.com>
2024-10-09 08:47:53 +0700

    feat: install Tailwind CSS and Livewire 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    'node_modules/preline/dist/*.js',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
 
